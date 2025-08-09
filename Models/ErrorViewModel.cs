ea2ac219c2adb17cf595bb74627962d10cd2a756
YelkcubNairb <31457411+YelkcubNairb@users.noreply.github.com>
2025-08-09 14:57:39 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GoEngineer_Star_Wars_API_Demonstration.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
