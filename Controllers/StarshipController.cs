66628f3769c209bbcdae6534cbd917f0052fea09
YelkcubNairb <31457411+YelkcubNairb@users.noreply.github.com>
2025-08-13 02:00:00 -0400

    Created the JS file to handle Ajax calls. Updated controller to handle CRUD functionality. Updates to views.

--- Controllers/StarshipController.cs
+++ Controllers/StarshipController.cs
@@ -12,7 +12,7 @@ namespace GalaxyFarFarAway.Controllers
         {
             _db = db;
         }
-        public IActionResult Index()
+        public ActionResult Index()
         {
             var starships = new List<StarshipViewModel>();
             starships = _db.Starships.ToList();
@@ -40,6 +40,125 @@ namespace GalaxyFarFarAway.Controllers
                 APIUrl = s.APIUrl
             }).ToList();
             return View("~/Views/Starship/Index.cshtml", vmList);
+        }
+
+        [HttpGet]
+        public ActionResult Upsert(int id)
+        {
+            if (id == 0)
+            {
+                return PartialView("~/Views/Starship/StarshipForm.cshtml", new StarshipViewModel());
+            }
+            var ship = _db.Starships.FirstOrDefault(s => s.Id == id);
+            if (ship != null)
+            {
+                var vm = new StarshipViewModel()
+                {
+                    Id = ship.Id,
+                    Name = ship.Name,
+                    Model = ship.Model,
+                    Manufacturer = ship.Manufacturer,
+                    CostInCredits = ship.CostInCredits,
+                    Length = ship.Length,
+                    MaxAtmospheringSpeed = ship.MaxAtmospheringSpeed,
+                    MinimumCrew = ship.MinimumCrew,
+                    MaximumCrew = ship.MaximumCrew,
+                    PassengerCapacity = ship.PassengerCapacity,
+                    CargoCapacity = ship.CargoCapacity,
+                    ConsumablesInYears = ship.ConsumablesInYears,
+                    HyperdriveRating = ship.HyperdriveRating,
+                    MegaLightPerHour = ship.MegaLightPerHour,
+                    StarshipClass = ship.StarshipClass,
+                    Pilots = ship.Pilots,
+                    Films = ship.Films,
+                    DateCreated = ship.DateCreated,
+                    DateLastUpdated = ship.DateLastUpdated,
+                    APIUrl = ship.APIUrl
+                };
+
+                return PartialView("~/Views/Starship/StarshipForm.cshtml", vm);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult Upsert(StarshipViewModel vm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (vm.Id == 0)
+            {
+                var newShip = new StarshipViewModel()
+                {
+                    Name = vm.Name,
+                    Model = vm.Model,
+                    Manufacturer = vm.Manufacturer,
+                    CostInCredits = vm.CostInCredits,
+                    Length = vm.Length,
+                    MaxAtmospheringSpeed = vm.MaxAtmospheringSpeed,
+                    MinimumCrew = vm.MinimumCrew,
+                    MaximumCrew = vm.MaximumCrew,
+                    PassengerCapacity = vm.PassengerCapacity,
+                    CargoCapacity = vm.CargoCapacity,
+                    ConsumablesInYears = vm.ConsumablesInYears,
+                    HyperdriveRating = vm.HyperdriveRating,
+                    MegaLightPerHour = vm.MegaLightPerHour,
+                    StarshipClass = vm.StarshipClass,
+                    Pilots = vm.Pilots,
+                    Films = vm.Films,
+                    DateCreated = DateTime.Now,
+                    DateLastUpdated = DateTime.Now,
+                };
+                _db.Starships.Add(newShip);
+            }
+            else
+            {
+                var ship = _db.Starships.FirstOrDefault(s => s.Id == vm.Id);
+                if (ship != null)
+                {
+                    ship.Name = vm.Name;
+                    ship.Model = vm.Model;
+                    ship.Manufacturer = vm.Manufacturer;
+                    ship.CostInCredits = vm.CostInCredits;
+                    ship.Length = vm.Length;
+                    ship.MaxAtmospheringSpeed = vm.MaxAtmospheringSpeed;
+                    ship.MinimumCrew = vm.MinimumCrew;
+                    ship.MaximumCrew = vm.MaximumCrew;
+                    ship.PassengerCapacity = vm.PassengerCapacity;
+                    ship.CargoCapacity = vm.CargoCapacity;
+                    ship.ConsumablesInYears = vm.ConsumablesInYears;
+                    ship.HyperdriveRating = vm.HyperdriveRating;
+                    ship.MegaLightPerHour = vm.MegaLightPerHour;
+                    ship.StarshipClass = vm.StarshipClass;
+                    ship.Pilots = vm.Pilots;
+                    ship.Films = vm.Films;
+                    ship.DateCreated = vm.DateCreated;
+                    ship.DateLastUpdated = DateTime.Now;
+                    ship.APIUrl = null;
+                    _db.Starships.Update(ship);
+                }
+
+            }
+            _db.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            var ship = _db.Starships.FirstOrDefault(s => s.Id == id);
+            if (ship != null)
+            {
+                _db.Starships.Remove(ship);
+                _db.SaveChanges();
+                return Ok();
+            }
+            return NotFound();
         }
 
         private readonly ApplicationDbContext _db;
---
+++ wwwroot/js/StarshipForm.js
@@ -0,0 +1,55 @@
+﻿$(document).ready(function () {
+
+    $("#addShip").on('click', function () {
+        $.get("/Starship/Upsert/0", function (html) {
+            $("#modalContent").html(html);
+            $("#starshipModal").modal("show");
+        });
+    });
+
+    $(document).on("click", "#btnEdit", function () {
+        var id = $(this).closest('tr').data("starship-id");
+        $.get("/Starship/Upsert/" + id, function (html) {
+            $("#modalContent").html(html);
+            $("#starshipModal").modal("show");
+        });
+    });
+
+    $(document).on("submit", "#starshipForm", function (e) {
+        e.preventDefault();
+
+        var form = $(this);
+
+        $.ajax({
+            url: form.attr("action"),
+            type: "POST",
+            data: form.serialize(),
+            success: function (res) {
+                $("#starshipModal").modal("hide");
+
+                location.reload();
+            },
+            error: function (xhr) {
+                $("#modalContent").html(xhr.responseText);
+            }
+        });
+    });
+
+    $(document).on("click", "#btnDelete", function () {
+        if (!confirm("Are you sure you want to delete this starship?")) return;
+
+        var id = $(this).closest('tr').data("starship-id");
+
+        $.ajax({
+            url: "/Starship/Delete/" + id,
+            type: "DELETE",
+            success: function () {
+                location.reload();
+            },
+            error: function () {
+                alert("Failed to delete the starship.");
+            }
+        });
+    });
+
+});
